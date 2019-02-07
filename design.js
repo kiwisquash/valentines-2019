d96e1c1a8c4fe39168cbe2d7ffe55f3d9ab47393
Ki Song <kiwisquash@gmail.com>
2019-02-06 20:52:16 -0800

    Improved fading mechanism by using callback.

--- design.js
+++ design.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
-$('h1').empty();
 // $('#message').append("Will you be my Lt. Hawkeye?");
 $('#oldMessage').append("testing");
 $('#newMessage').append("hello");
 $('#newMessage').hide();
-$('#coupons').hide()
+$('.prize').hide()
 $('#mushy').hide();
 $("#yes").click(function(){
-    $("#oldMessage").fadeOut("slow");
-    $("#oldMessage").hide();
-    $("#newMessage").fadeIn("slow");
-    // $("h1").append("Happy Valentine's Day!");
-    $("#buttons").fadeOut("slow");
-    $("#buttons").hide();
-    $("#coupons").fadeIn("slow");
+    $("#oldMessage").fadeOut("slow", function(){
+        $("#newMessage").fadeIn("slow");
+        $("#buttons").fadeOut("slow", function(){
+            $(".prize").fadeIn("slow");
+        });
+    });
   });
+$("img").click(function(){
+    $(this).fadeOut();
+});
 });
 
-$(".prize").click(function(){
-    $("this").fadeOut("slow");
-});
+
 // var productImage = $('img');
 // var productOverlay = $('.product-image-overlay');
 // var productOverlayImage = $('.product-image-overlay img');
